3ccb97823ec7c2cdcc5cf7ee4479ff0fd408da2a
Jan Zadumiński <jan.zaduminski@unicard-systems.pl>
2024-06-24 20:28:05 +0200

    Login page + page element

--- cypress/e2e/panel/order-service/panel-order-service-test.spec.ts
+++ cypress/e2e/panel/order-service/panel-order-service-test.spec.ts
@@ -3,13 +3,14 @@ import {QueryAssertion} from "../../../support/beeoclock/common/assertion/QueryA
 import {PanelLoginPage} from "../../../support/beeoclock/page-element/configuration/login/page-element/PanelLoginPage";
 import {ClientPropertiesEnum} from "../../../support/beeoclock/common/enum/ClientPropertiesEnum";
 import {PanelLoginPageElement} from "../../../support/beeoclock/page-element/configuration/login/PanelLoginPageElement";
+import {RightPanelPages} from "../../../support/beeoclock/page-element/configuration/right-panel/RightPanelPages";
 
 describe('panel - order service', () => {
 
     it('test panel  order service',  function () {
         cy.intercept('GET', '**/*').as('getAll');
         cy.visit(ServiceEnum.CLIENT_PANEL, { failOnStatusCode: false });
-        cy.wait('@getAll', { timeout: 30000 }); // Wydłużenie czasu oczekiwania na przechwycone żądania
+        cy.wait('@getAll', { timeout: 30000 });
 
 
         PanelLoginPageElement.EmailInput.getElement()
@@ -18,7 +19,14 @@ describe('panel - order service', () => {
         PanelLoginPage.typePassword(ClientPropertiesEnum.PASSWORD)
         PanelLoginPage.clickLoginButton()
 
-
         QueryAssertion.verifyCorrectUrl('/event/calendar-with-specialists')
+
+        RightPanelPages.RightPanelNavigationPage
+            .clickOpenRightPanel()
+        RightPanelPages.RightPanelServicesPage
+            .clickAddServiceButton()
+            .clickSelectServiceButton()
+            .selectSpecificService('Strzyżenie Brody')
+            .verifySelectedService('Strzyżenie Brody', 'Samo Strzyżenie Brody')
     })
 })
--- cypress/support/beeoclock/page-element/configuration/right-panel/navigation/page-element/OpenRightPanelButton.ts
+++ cypress/support/beeoclock/page-element/configuration/right-panel/navigation/page-element/OpenRightPanelButton.ts
@@ -1,7 +1,6 @@
 export class OpenRightPanelButton {
     public getElement(): any {
         return cy.get('.p-2.flex.justify-between').find('.bi.bi-plus-lg')
-            .contains('i')
             .parent('button')
             .scrollIntoView()
     }
