247b4ef6a632bba89c40cd4dca059bb372ae6b6a
Jan Zadumiński <jan.zaduminski@unicard-systems.pl>
2024-08-31 08:48:32 +0200

    absence test fix order test failure debug

--- cypress/support/beeoclock/page-element/configuration/tab/order-tab/actions/page-element/OrderActionButton.ts
+++ cypress/support/beeoclock/page-element/configuration/tab/order-tab/actions/page-element/OrderActionButton.ts
@@ -4,18 +4,18 @@
 //         return cy.get(`#table-row-${orderId}`)
 //     }
 //this get just main component
-// export class OrderActionButton {
-//     public getElement(orderId: string): any {
-//         return cy.get(`app-card-item-order-component[id="${orderId}"]`)
-//             .scrollIntoView().should('be.visible')
-//     }
-// }
-
 export class OrderActionButton {
     public getElement(orderId: string): any {
-        // @ts-ignore
-        return cy.get(`#\\36 ${orderId} > .border-2 > :nth-child(1) > .flex-col.cursor-pointer > app-list-service-form-card-order-component.flex > :nth-child(1) > .bg-white > .flex-col > .gap-1 > .w-8`)
+        return cy.get(`app-card-item-order-component[id="${orderId}"]`)
             .scrollIntoView().should('be.visible')
     }
-
 }
+
+// export class OrderActionButton {
+//     public getElement(orderId: string): any {
+//         // @ts-ignore
+//         return cy.get(`#\\36 ${orderId} > .border-2 > :nth-child(1) > .flex-col.cursor-pointer > app-list-service-form-card-order-component.flex > :nth-child(1) > .bg-white > .flex-col > .gap-1 > .w-8`)
+//             .scrollIntoView().should('be.visible')
+//     }
+
+// }
