7413d063438268223749833e5db023393dfa0745
Janek <janekzaduminski@gmail.com>
2024-09-25 20:15:55 +0200

    maintenance test fixes in progress

--- cypress/support/beeoclock/common/enum/ServiceEnum.ts
+++ cypress/support/beeoclock/common/enum/ServiceEnum.ts
@@ -1,5 +1,5 @@
 export enum ServiceEnum {
 
-    PUBLIC_PANEL = 'https://public-dev-beeoclock--pr104-payment-at-order-cre-lzghb8dx.web.app/e2e',
+    PUBLIC_PANEL = 'https://dev.beeoclock.com/e2e',
     CLIENT_PANEL = 'https://panel.dev.beeoclock.com/identity',
 }
