ac9fa93f29427a2b8bc675a19a080e370d76f15e
Karba <ivan.karbashevskyi@gmail.com>
2024-10-14 20:37:42 +0200

    UPD

--- cypress/support/beeoclock/common/enum/ServiceEnum.ts
+++ cypress/support/beeoclock/common/enum/ServiceEnum.ts
@@ -1,6 +1,6 @@
 export enum ServiceEnum {
 
-    PUBLIC_PANEL = 'https://dev.beeoclock.com/e2e',
+    PUBLIC_PANEL = 'https://dev.beeoclock.com/pl/e2e',
     CLIENT_PANEL = 'https://panel.dev.beeoclock.com/662a4637a4b376d20c065b1d/event/calendar-with-specialists',
     // CLIENT_PANEL = 'https://business-panel-dev-beeoclock--pr527-issue-525-q1mjfwxk.web.app/',
 }
