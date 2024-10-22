cd97046b25d3c2c6f48f077eee15ba70f20ed855
Janek <janekzaduminski@gmail.com>
2024-10-22 20:35:35 +0200

    22.10 fix v2

--- cypress/support/beeoclock/backend/enum/BackendCommonEnum.ts
+++ cypress/support/beeoclock/backend/enum/BackendCommonEnum.ts
@@ -1,6 +1,6 @@
 export enum BackendCommonEnum {
 
     ENTRY_POINT = 'https://api.dev.beeoclock.com/panel/api/v1/',
-    // X_Business_Tenant_Id = '662a4637a4b376d20c065b1d'
-    X_Business_Tenant_Id = '6714ec77354a8d568ed83b54'
+    X_Business_Tenant_Id = '662a4637a4b376d20c065b1d'
+    // X_Business_Tenant_Id = '6714ec77354a8d568ed83b54'
 }
