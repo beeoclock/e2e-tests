d86c5cc722f7c00a37d08eeb24c2cb5f3acf7235
Jan Zadumiński <jan.zaduminski@unicard-systems.pl>
2024-11-05 17:25:41 +0100

    service public_panel enum change to default

--- cypress/support/beeoclock/common/enum/ServiceEnum.ts
+++ cypress/support/beeoclock/common/enum/ServiceEnum.ts
@@ -1,6 +1,6 @@
 export enum ServiceEnum {
 
-    PUBLIC_PANEL = 'https://public-dev-beeoclock--pr111-multiple-select-serv-5r66exbt.web.app/pl/e2e',
+    PUBLIC_PANEL = 'https://dev.beeoclock.com/pl/e2e',
 
     //default
     // CLIENT_PANEL = 'https://panel.dev.beeoclock.com/662a4637a4b376d20c065b1d/event/calendar-with-specialists',
