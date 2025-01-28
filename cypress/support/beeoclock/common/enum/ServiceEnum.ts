709f7fb422c584b452bb9a5ef72e929daf9c3dac
Jan Zadumiński <jan.zaduminski@unicard-systems.pl>
2025-01-28 08:48:49 +0100

    enums

--- cypress/support/beeoclock/common/enum/ServiceEnum.ts
+++ cypress/support/beeoclock/common/enum/ServiceEnum.ts
@@ -2,10 +2,10 @@ export enum ServiceEnum {
 
     //develop
     PUBLIC_PANEL = 'https://dev.beeoclock.com/pl/e2e',
-    CLIENT_PANEL = 'https://panel.dev.beeoclock.com/662a4637a4b376d20c065b1d/event/calendar-with-specialists',
+    // CLIENT_PANEL = 'https://panel.dev.beeoclock.com/662a4637a4b376d20c065b1d/event/calendar-with-specialists',
 
     //PR
     // PUBLIC_PANEL = 'https://beeoclock--pr120-develop-j2tplmgj.web.app/pl/e2e',
-    // CLIENT_PANEL = 'https://panel-beeoclock--pr576-develop-5jbsrh9q.web.app/6714ec77354a8d568ed83b54/event/calendar-with-specialists',
+    CLIENT_PANEL = 'https://business-panel-dev-beeoclock--pr581-boc-151-bk1lonpe.web.app/662a4637a4b376d20c065b1d/event/calendar-with-specialists',
     //https://beeoclock--pr120-develop-j2tplmgj.web.app/pl/e2e/order/form
 }
