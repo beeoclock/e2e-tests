1a490b30f9778d3c6ae83fa8ef1e5caec2be12ee
Jan Zadumiński <jan.zaduminski@unicard-systems.pl>
2025-02-18 08:39:30 +0100

    token as global env

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -12,29 +12,26 @@
 // You can read more here:
 // https://on.cypress.io/configuration
 // ***********************************************************
-require('cypress-xpath');
-
+import {AuthApi} from "./beeoclock/backend/auth/AuthApi";
 // Import commands.js using ES2015 syntax:
 import './commands';
 import 'cypress-file-upload';
 
+require('cypress-xpath');
+
+/**
+ * clear all storage and assign to global env valid token before any test
+ */
 before(() => {
-    clearAllData();
+    clearAllData()
+
+    AuthApi.getToken().then(token => {
+        Cypress.env('token', token);
+    });
 });
 
 function clearAllData() {
     cy.clearCookies();
     cy.clearLocalStorage();
     cy.clearAllSessionStorage()
-
-    cy.window().then((win) => {
-        if (win.caches) {
-            win.caches.keys().then((cacheNames) => {
-                cacheNames.forEach((cacheName) => {
-                    win.caches.delete(cacheName);
-                });
-            });
-        }
-        win.sessionStorage.clear();
-    });
 }
