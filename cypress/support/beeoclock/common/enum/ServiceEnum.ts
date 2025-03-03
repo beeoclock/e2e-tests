0a1fb454af8ac42aa9b14592ca61a1389cb63d89
Jan Zadumiński <jan.zaduminski@unicard-systems.pl>
2025-03-03 13:43:47 +0100

    public page entry url fix

--- cypress/support/beeoclock/common/enum/ServiceEnum.ts
+++ cypress/support/beeoclock/common/enum/ServiceEnum.ts
@@ -1,7 +1,7 @@
 export enum ServiceEnum {
 
     //develop
-    PUBLIC_PANEL = 'https://dev.beeoclock.com/662a4637a4b376d20c065b1d',
+    PUBLIC_PANEL = 'https://dev.beeoclock.com/pl/e2e',
     CLIENT_PANEL = 'https://panel.dev.beeoclock.com/662a4637a4b376d20c065b1d/event/calendar-with-specialists',
     PRODUCT_CLIENT_PANEL = 'https://panel.beeoclock.com/662a4637a4b376d20c065b1d/event/calendar-with-specialists',
 
