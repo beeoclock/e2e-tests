0d033e25543325785d31441689d25f1233d10ac8
Janek <janekzaduminski@gmail.com>
2025-03-16 22:32:59 +0100

    create product tag and delete skip on gh action

--- cypress/e2e/backend/healthcheck/api-healthcheck-test.spec.ts
+++ cypress/e2e/backend/healthcheck/api-healthcheck-test.spec.ts
@@ -68,7 +68,7 @@ describe("panel api healthcheck", () => {
         })
     });
 
-    it('create product tag and delete', function (): void {
+    it.skip('create product tag and delete', function (): void {
         let id: string = NumericUtils.generateObjectId()
         const tag = new ProductTagBuilder().setId(id).setName('TAG NO ' + faker.finance.pin(6)).build();
         ProductApi.createProductTag(tag, token).then(() => {
