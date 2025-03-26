56fcde346afec72c373977dc4591e9ae158cc7b4
Janek <janekzaduminski@gmail.com>
2025-03-26 19:44:27 +0100

    logs

--- cypress/e2e/backend/auth/token.spec.ts
+++ cypress/e2e/backend/auth/token.spec.ts
@@ -1,12 +1,10 @@
 import { AuthApi } from "../../../support/beeoclock/backend/auth/AuthApi";
 
 describe('token', () => {
-
     it('get token and log it', () => {
-        //
         AuthApi.getToken().then((token) => {
+            expect(token).to.not.be.empty;
             Cypress.env('token', token);
-
             cy.log('Token:', token);
         });
     });
