e531cd79891badb3f87e9c11c8e995fda876e299
Jan Zadumiński <jan.zaduminski@unicard-systems.pl>
2025-03-26 16:12:30 +0100

    log false delete

--- cypress/support/beeoclock/common/Interception/EntryPointEnum.ts
+++ cypress/support/beeoclock/common/Interception/EntryPointEnum.ts
@@ -1,11 +1,6 @@
 export enum EntryPointEnum {
-    //default
     API_ENTRY_POINT = 'https://api-dev.beeoclock.com/panel/api/v1',
     TARIFFS_ENTRY_POINT = 'https://api-dev.beeoclock.com/tariff-plan/api/v1',
-
-///////////////////////https://api-dev.beeoclock.com/panel/api/v1/business-profile
-    //PR
-    // API_ENTRY_POINT = 'https://api.beeoclock.com/panel/api/v1',
     PUBLIC_PAGE_API_ENTRY_POINT = 'https://api-dev.beeoclock.com/client/api/v1/client/e2e',
 
 }
