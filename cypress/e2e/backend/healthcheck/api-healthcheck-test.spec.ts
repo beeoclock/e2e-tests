4fa842c6f6911ffc356d7a87adf1c36fef7f08c2
Janek <janekzaduminski@gmail.com>
2025-03-26 18:50:07 +0100

    token in headers

--- cypress/e2e/backend/healthcheck/api-healthcheck-test.spec.ts
+++ cypress/e2e/backend/healthcheck/api-healthcheck-test.spec.ts
@@ -56,7 +56,7 @@ describe("panel api healthcheck", () => {
         })
     });
 
-    it('create product tag and delete', function (): void {
+    it.only('create product tag and delete', function (): void {
         let id: string = NumericUtils.generateObjectId()
         const tag = new ProductTagBuilder().setId(id).setName('TAG NO ' + faker.finance.pin(6)).build();
         ProductApi.createProductTag(tag, token).then(() => {
