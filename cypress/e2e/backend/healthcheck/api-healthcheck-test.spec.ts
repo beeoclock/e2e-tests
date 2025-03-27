11609ee1fddba95086af35d12c3d6f2e852f9a60
Jan Zadumiński <jan.zaduminski@unicard-systems.pl>
2025-03-27 15:12:52 +0100

    all action enabled

--- cypress/e2e/backend/healthcheck/api-healthcheck-test.spec.ts
+++ cypress/e2e/backend/healthcheck/api-healthcheck-test.spec.ts
@@ -47,7 +47,7 @@ describe("panel api healthcheck", () => {
         });
     });
 
-    it.only('create product tag and delete', function (): void {
+    it('create product tag and delete', function (): void {
         let id: string = NumericUtils.generateObjectId()
         const tag = new ProductTagBuilder().setId(id).setName('TAG NO ' + faker.finance.pin(6)).build();
         ProductApi.createProductTag(tag, token).then(() => {
