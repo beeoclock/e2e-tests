50cd44dfaae4d14ce364e8d49b3d9df22e28b9a9
Janek <janekzaduminski@gmail.com>
2025-09-07 20:28:08 +0200

    product api

--- cypress/support/beeoclock/backend/panel/member/MemberApi.ts
+++ cypress/support/beeoclock/backend/panel/member/MemberApi.ts
@@ -1,7 +1,6 @@
-import {ApiRequestHelper} from "../../../common/Interception/ApiRequestHelper";
+import {ApiRequestHelper, Environment} from "../../../common/Interception/ApiRequestHelper";
 import {CustomerSearchCriteriaBuilder} from "../customer/queries/CustomerSearchCriteriaBuilder";
 import {HttpMethodEnum} from "../../../common/enum/HttpMethodEnum";
-import {HTTPStatusCodeType} from "../../enum/HTTPStatusCodeType";
 
 export class MemberApi extends ApiRequestHelper {
 
@@ -12,7 +11,7 @@ export class MemberApi extends ApiRequestHelper {
             .withPage(1)
             .withPageSize(10)
             .build()
-        return this.handleApiQueryRequest('/member/paged', qs)
+        return this.handleApiQueryRequest(Environment.dev, '/member/paged', qs)
     }
 
     public static updateMember(memberId: string, member: any): any {
@@ -20,6 +19,6 @@ export class MemberApi extends ApiRequestHelper {
             ...member,
         };
 
-        return this.handleApiRequest(HttpMethodEnum.PUT, `/member/${memberId}`, updatedMember)
+        return this.handleApiRequest(Environment.dev, HttpMethodEnum.PUT, `/member/${memberId}`, updatedMember)
     }
 }
